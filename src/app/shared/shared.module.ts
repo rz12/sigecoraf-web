8eb633eb62030b8d69a1c7a4520b4374694c4074
rz12 <rz416296@gmail.com>
2018-03-16 10:45:09 -0500

    *crear componente dialog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { DialogComponent } from './componets/dialog/dialog.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  declarations: []
+  declarations: [DialogComponent]
 })
 export class SharedModule { }
