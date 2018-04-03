aaca8534e4360c1cefd87e6949afd669c85f4cd0
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2018-04-03 11:36:09 -0500

    Desarrollo de dashboard

---
+++ src/app/nominas/dto/i-cargo.ts
@@ -0,0 +1,5 @@
+import { Cargo } from "../models/cargo";
+export interface ICargo {
+    data: Cargo[]
+}
+
---
+++ src/app/nominas/models/cargo.ts
@@ -0,0 +1,8 @@
+import { Empresa } from "../../master/models/empresa";
+export class Cargo {
+    private nombre: String;
+    private descripcion: String;
+    private estado: Boolean;
+    private empresa: Empresa;
+    private sueldo: Number;
+}
