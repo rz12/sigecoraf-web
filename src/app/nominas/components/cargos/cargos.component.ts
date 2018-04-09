a4c91190aa471ff8336e4f1997d38468f138fbc2
Jorge Luis Malla Sánchez <jomasloja@gmail.com>
2018-04-09 10:54:08 -0500

    Actualizacion de seguridad para acceso a menus

--- src/app/nominas/components/cargos/cargos.component.ts
+++ src/app/nominas/components/cargos/cargos.component.ts
@@ -3,6 +3,7 @@ import { CargoService } from '../../services/cargo.service';
 import { Cargo } from '../../models/cargo';
 import { SeguridadService } from '../../../seguridad/services/seguridad.service';
 import { MatTableDataSource } from '@angular/material';
+import { enums } from '../../../credentials';
 
 @Component({
   selector: 'app-cargos',
@@ -15,11 +16,19 @@ export class CargosComponent implements OnInit {
   dataSource = new MatTableDataSource();
   public urlEdit = "cargo-detail/0"
   public urlAdd = "cargo-detail/0"
+  public message: String;
   constructor(private cargoService: CargoService, private seguridadService: SeguridadService) { }
 
   ngOnInit() {
     let token = this.seguridadService.getToken()
-    this.cargoService.cargosList(token.token).subscribe(data => this.dataSource.data = this.cargosList = data.data);
+    this.cargoService.cargosList(token.token).subscribe(data => {
+
+      if (data.json().status == enums.HTTP_200_OK) {
+        this.dataSource.data = this.cargosList = data.json().data
+      } else if (data.json().status == enums.HTTP_401_UNAUTHORIZED) {
+        this.message = data.json().message;
+      }
+    });
   }
 
 }
--- src/app/nominas/services/cargo.service.ts
+++ src/app/nominas/services/cargo.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { services } from "../../credentials";
 import { ICargo } from "../dto/i-cargo";
 import { SharedService } from '../../shared/services/shared.service';
 import { catchError, map } from 'rxjs/operators';
+import { Http, Headers, RequestOptions } from "@angular/http";
 @Injectable()
 export class CargoService extends SharedService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: Http) {
     super();
   }
 
-  cargosList(token): Observable<ICargo> {
-    let headers = new HttpHeaders();
+  cargosList(token) {
+    let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
     headers.append("Authorization", token);
-    let httpOptions = {
-      headers: headers
-    };
-    return this.http.get<ICargo>(services.ws_nominas_cargos, httpOptions)
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(services.ws_nominas_cargos, options)
   }
   save() {
 
